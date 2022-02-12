a39a38779a0db5b5ddc3ece57fd24573d6f62f56
Julian <slonim.julian@gmail.com>
2022-02-12 00:41:47 -0300

    initial commit

---
+++ CleanRun/Commands/MyCommand.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+
+namespace CleanRun
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            await VS.Build.BuildSolutionAsync(BuildAction.Clean);
+            (await VS.Services.GetSolutionBuildManagerAsync()).DebugLaunch((uint)VSSOLNBUILDUPDATEFLAGS.SBF_OPERATION_LAUNCHDEBUG);
+        }
+    }
+}
